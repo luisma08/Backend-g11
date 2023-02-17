7c2752d4583a54cc947cd7d0b92e6ba43468b026
Luis <luismayta08@gmail.com>
2023-02-16 21:10:02 -0500

    s.1 creando archivo introduccion.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Repositorio de Backend de Codigo G11
 
-<p align>
+<p>
     <img src="https://assets.website-files.com/624b2bd5b7be89e20392d489/624b37b08ca87609798e03a6_codigo-logo-blanco.svg" />
 </p>
 
---
+++ introduccion.py
@@ -0,0 +1,22 @@
+numero = 10
+otro_numero = 20
+
+if numero > 11:
+    print('Este numero es mayor que 11')
+
+string = "Soy un string"
+boolean_true = True
+boolean_false = False
+flotantes = 3.14
+diccionaios = {
+    'nombre0': 'Eduardo'
+}
+listas = [1,2,3,'string', 3.14]
+# print(listas)
+
+edad = int(input('Ingresa tu edad: '))
+
+if edad < 18:
+    print('Eres menor de edad')
+else:
+    print('Eres mayor de edad')
