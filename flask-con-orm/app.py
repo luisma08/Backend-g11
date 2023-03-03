4914d0ae805b3d101ec855a0eab43776c7389bba
Luis <luismayta08@gmail.com>
2023-03-02 23:10:56 -0500

    creando variable de entorno .env y conectando a traves de flask_sqlalchemy

---
+++ flask-con-orm/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from base_de_datos import conexion
+from dotenv import load_dotenv
+from os import environ
+
+# Es el encargado de leer el archivo .env si es que existe y agregar las variables en ese archivo como si fueran variables de entorno
+load_dotenv()
+
+app = Flask(__name__)
+# dialect://username:password@host:port/database
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+
+# Si quiero crear mi conexion en otro archivo e inicializar la configuracion de mi conexion, 
+# tengo que utilizar el metodo init_app y es aca donde le pasare el parametro de mi instancia
+# de la clase Flask
+conexion.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
