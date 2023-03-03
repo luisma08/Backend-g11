fe17c013275a12891994184a86fe60ce03a0d70f
Luis <luismayta08@gmail.com>
2023-03-02 20:24:27 -0500

    clase02 update database

--- Clase-01/flask-postgres.py
+++ Clase-01/flask-postgres.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from psycopg2 import connect
 
 app = Flask(__name__)
+# me conecto a la bd
+conexion = connect(host='localhost', database = 'pruebas', user = 'postgres', password = 'masterPGsql15*')
 
 # Creamos decorador
 @app.route('/', methods = ['GET'])
@@ -13,8 +15,6 @@ def inicial():
 @app.route('/alumnos', methods = ['GET', 'POST'])
 def alumnos():
     if request.method == 'GET':
-        # me conecto a la bd
-        conexion = connect(host='localhost', database = 'pruebas', user = 'postgres', password = 'masterPGsql15*')
 
         # Creo un cursor que es el responsable de hacer lecturas y escrituras a la db
         cursor = conexion.cursor()
@@ -25,14 +25,71 @@ def alumnos():
         #Extraigo la informacion de la ejecucion de la query
         resultado = cursor.fetchall()
         print(resultado)
+        alumnos_resultado = []
 
+        for alumno in resultado:
+            infor_alumno = {
+                'id': alumno[0],
+                'nombre': alumno[1],
+                'apellido': alumno[2],
+                'sexo': alumno[3],
+                'fecha_creacion': alumno[4],
+                'matriculado': alumno[5]
+            }
+            alumnos_resultado.append(infor_alumno)
         return {
-            'message': 'Yo soy el GET'
+            'message': alumnos_resultado
         }
     elif request.method == 'POST':
+        data = request.json
+        print(data)
+        cursor = conexion.cursor()
+        cursor.execute("INSERT INTO ALUMNOS (nombre, apellido, matriculado) VALUES (%s, %s, %s)",(
+            data.get('nombre'), data.get('apellido'), data.get('matriculado')
+        ))
+
+        # Indicamos a la base de datos que guarde los cambios (insert) de manera permanente
+        conexion.commit()
+
         return {
-            'message': 'Yo soy el POST'
+            'message': 'Alumno ingresado exitosamente'
         }
+    
+@app.route('/alumno/<id>', methods = ['GET', 'PUT', 'DELETE'])
+def gestion_alumnos(id):
+    if request.method == 'GET':
+        # Creo un cursor que es el responsable de hacer lecturas y escrituras a la db
+        cursor = conexion.cursor()
+
+        # Ejecuto la query
+        cursor.execute('SELECT * FROM alumnos WHERE id = %s', (id,))
+        alumno = cursor.fetchone()
+
+        if alumno:
+            # Significa que el alumno existe
+            return {
+                'content': {
+                'id': alumno[0],
+                'nombre': alumno[1],
+                'apellido': alumno[2],
+                'sexo': alumno[3],
+                'fecha_creacion': alumno[4],
+                'matriculado': alumno[5]
+                }
+            }
+        
+        else:
+            return {
+                'message': 'El alumno no existe'
+            }
+    elif request.method == 'PUT':
+        # Recibir la informacion del body y el id por la url y modificar la data del alumno, primero validar si el alumno existe no hacer modificacion, 
+        # si no existe no hacer ninguna modificacion, si existe hacer la modificacion
+        pass
+
+    elif request.method == 'DELETE':
+        # Recibir el id por la url y validar si el alumno existe, si existe, eliminarlo (hacer un delete) caso contrario indicar que el alumno no existe
+        pass
 
 if __name__ == '__main__':
     # debug indica que cada vez que guardemos un archivo del proyecto el servidor se reinicie automaticamente
