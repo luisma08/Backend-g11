ad0618341165e3fb32038b06f0e91abbf4ccb4cd
Luis <luismayta08@gmail.com>
2023-03-03 19:42:40 -0500

    actualizando nombre de clase maestro

--- flask-con-orm/models/maestro_model.py
+++ flask-con-orm/models/maestro_model.py
@@ -1,7 +1,7 @@
 from base_de_datos import conexion
 from sqlalchemy import Column, types
 
-class Seccion(conexion.Model):
+class Maestro(conexion.Model):
     id = Column(type_=types.Integer, primary_key=True, autoincrement=True)
     nombre = Column(type_=types.Text, nullable=False)
     apellido = Column(type_=types.Text)
