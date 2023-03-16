abd9966a2132fa90616e03eb7f9b7342e345d12c
Luis <luismayta08@gmail.com>
2023-03-15 21:32:47 -0500

    creando modelo usuario

---
+++ models/usuario_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, types
+from enum import Enum
+from db import conexion
+
+class TipoUsuario(Enum):
+    ADMIN = 'ADMIN'
+    PERSONA = 'PERSONA'
+
+class Usuario(conexion.Model):
+    id = Column(type_= types.Integer, autoincrement=True, primary_key=True)
+    nombre = Column(type_=types.Text, nullable=False)
+    apellido = Column( type_=types.Text)
+    correo = Column(type_= types.Text, unique=True, nullable=False)
+    password = Column(type_=types.Text, nullable=False)
+    tipo = Column(type_=types.Enum(), default=TipoUsuario.PERSONA)
+
+    __tablename__ = 'usuarios'
