219dc96a408f137b37d9547fd00ab483f0636ff4
Luis <luismayta08@gmail.com>
2023-03-15 22:52:31 -0500

    creando controlador y migrando tabla usuarios

---
+++ dtos/usuario_dto.py
@@ -0,0 +1,7 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from models.usuario_model import Usuario
+
+class UsuarioDto(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
