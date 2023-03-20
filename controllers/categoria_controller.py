75c61f3c15b8d00e5a22e8a2e7fbe8a52202e270
Luis <luismayta08@gmail.com>
2023-03-19 23:43:29 -0500

    creando metodo post y get en categoria controller

--- controllers/categoria_controller.py
+++ controllers/categoria_controller.py
@@ -2,10 +2,12 @@ from flask_restful import Resource, request
 from flask import send_file
 from werkzeug.utils import secure_filename
 from os import path
+from sqlalchemy.orm import Query
 # universal unique indentifier
 from uuid import uuid4
 from dtos.categoria_dto import CategoriaDto
 from models.categoria_model import Categoria
+from db import conexion
 
 class ImagenesController(Resource):
     def post(self):
@@ -28,10 +30,49 @@ class ImagenesController(Resource):
         
 class CategoriasController(Resource):
     def post(self):
-        data = request.form
-        # Vamos a recibir la imagen mediante la llave llamada imagen
-        imagen = request.files.get('imagen')
-        pass
+        # mimetype_validos = ['image/svg+xml', 'image/webp', 'image/png', 'image/jpeg']
+        mimetype_valido = 'image/'
+        data = request.form.to_dict()
+        try:
+            # vamos a recibir la imagen mediante la llave llamada imagen
+            imagen = request.files.get('imagen') 
+            print(imagen.filename)
+            if  mimetype_valido not in imagen.mimetype:
+                raise Exception('El archivo no es un archivo valido')
+
+            dto = CategoriaDto()
+            # modifica la imagen agregando un identificador para evitar sobre escrituras
+            nombre = secure_filename(uuid4().hex +'_'+ imagen.filename)
+
+            # agrego a mi formulario el nombre de la imagen que me envia el cliente
+            # agregamos la carpeta donde se almacenara la imagen y con el nuevo nombre
+            data['imagen'] = 'imagenes/' + nombre
+            data_serializada = dto.load(data)
+
+            nueva_categoria = Categoria(**data_serializada)
+            conexion.session.add(nueva_categoria)
+
+            # guardo la imagen en nuestro servidor
+            imagen.save(path.join('imagenes', nombre))
+
+            conexion.session.commit()
+            return {
+                'message': 'Categoria creada exitosamente'
+            }
+        except Exception as error:
+            conexion.session.rollback()
+            return {
+                    'message': 'Error al crear la categoria',
+                    'content': error.args
+                }
 
     def get(self):
-        pass
+        query: Query = conexion.session.query(Categoria)
+        resultado = query.all()
+        dto = CategoriaDto()
+        
+        data = dto.dump(resultado, many=True)
+
+        return {
+            'content': data
+        }
